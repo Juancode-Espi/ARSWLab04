d0cd1b4924ce204650ba39c63cea767cce106dd5
unknown <juan.espinosa-t@labinfo.is.escuelaing.edu.co>
2022-02-09 09:26:21 -0500

    Introduccion completada

---
+++ Spring_LightweightCont_Annotation-DI_Example-master/src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -0,0 +1,17 @@
+package edu.eci.arsw.springdemo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EnglishSpellChecker implements SpellChecker {
+        
+        
+	@Override
+	public String checkSpell(String text) {		
+		return "Checked with english checker:"+text;
+	}
+
+        
+}
